5e0511f31ab1de241b86944e2837a1e53a770011
闫忠心 <18861812263@163.com>
2020-10-26 22:04:14 +0800

    新增了syn的锁消除，粗化，嵌套所，自旋锁

--- out/src/Main.java
+++ out/src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
-
+    private static final Object obj=new Object();
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    }
+
+    public static void test(){
+
+        synchronized (obj){
+            synchronized (obj){
+                System.out.println("synchronized");
+            }
+        }
+    }
+
+
+
+    public void lockEliminate(){
+
+        synchronized (obj){
+            // to do
+        }
     }
 }
